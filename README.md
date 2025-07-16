5b07b7ff02cd91f7931806e2377c8dab3c57e162
Vladislav Pavlovskii <walabubu@gmail.com>
2025-07-16 13:02:12 +0100

    Proof Of Concept implementation

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# cs-challenge
-CraftingSoftware coding challenge
+cs_api
+=====
+
+This is a Proof-Of-Concept implementation. It focuses on the algorithmic task of job orchestration and provides a very basic implementation of the HTTP layer. There's no external dependencies, this POC implementation is based on the standard libraries only.
+
+Build
+-----
+
+You need the Erlang runtime **and** rebar3 to be properly installed and added to the PATH
+
+    $ make
+
+Test
+-----
+
+    $ make test
+
+Run
+-----
+
+This will start the service in Erlang shell. By default the service listens at port 8000. You can modify [erl.config](erl.config) to use a different port number.
+
+    $ make shell
+
+Query
+-----
+
+Get response in JSON format (default) using [tasks.json](tasks.json) as the request body:
+
+    $ curl -X PUT http://localhost:8000/cs/cs_api_http:handle -d @tasks.json
+
+Get response in JSON format (explicitly):
+
+    $ curl -X PUT http://localhost:8000/cs/cs_api_http:handle/json -d @tasks.json
+
+Get response as a bash script:
+
+    $ curl -X PUT http://localhost:8000/cs/cs_api_http:handle/bash -d @tasks.json
---
+++ src/cs_api_http.erl
@@ -0,0 +1,67 @@
+%%------------------------------------------------------------------------------
+%% cs_api_http: CraftingSoftware coding challenge HTTP API module
+%%------------------------------------------------------------------------------
+-module(cs_api_http).
+-behaviour(mod_esi).
+-export([handle/3]).
+
+%%==============================================================================
+%% API functions
+%%==============================================================================
+
+handle(SessionID, Env, {_, Body}) ->
+    Path = proplists:get_value(path_info, Env),
+    OutputFormat = case parse_path(Path) of
+        []       -> json;
+        ["json"] -> json;
+        ["bash"] -> bash;
+        _        -> invalid        
+    end,
+    case OutputFormat of
+        invalid ->
+            bad_request(SessionID, "Invalid output format: " ++ Path);
+        _ ->
+            try
+                Request = json:decode(list_to_binary(Body)),
+                case cs_api:process_request(Request, OutputFormat) of
+                    {ok, Response} ->
+                        case OutputFormat of
+                            json -> json_response(SessionID, Response);
+                            bash -> text_response(SessionID, Response)
+                        end;
+                    {error, Error} -> bad_request(SessionID, Error)
+                end
+            catch
+                error:_ ->
+                    bad_request(SessionID, "Invalid request format")
+            end
+    end;
+handle(SessionID, _Env, _Input) ->
+	bad_request(SessionID, "Invalid request format").
+
+%%==============================================================================
+%% Internal functions
+%%==============================================================================
+
+parse_path(undefined) ->
+	[];
+parse_path(Path) ->
+    lists:delete([], string:split(Path, "/", all)).
+
+%% HTTP response building helpers
+
+bad_request(SessionID, Message) ->
+    mod_esi:deliver(SessionID, [
+        "status: 400 Bad Request\r\n",
+        ct_header("text/plain"),
+        Message
+    ]).
+
+json_response(SessionID, Data) ->
+    mod_esi:deliver(SessionID, [ct_header("application/json"), Data]).
+
+text_response(SessionID, Data) ->
+    mod_esi:deliver(SessionID, [ct_header("text/plain"), Data]).
+
+ct_header(MimeType) ->
+    "Content-type: " ++ MimeType ++ "\r\n\r\n".
